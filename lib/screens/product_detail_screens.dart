a094fcdf72a0606ef2fbd881f64f1ab4dcc063d6
Mugi <mugypleci@gmail.com>
2019-10-10 22:15:47 +0700

    listening to data in differen places and ways (finding data by id)

--- lib/screens/product_detail_screens.dart
+++ lib/screens/product_detail_screens.dart
@@ -1,23 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = '/product-detail';
 
   @override
   Widget build(BuildContext context) {
-    final productId = ModalRoute.of(context).settings.arguments as String;
-    print(productId);
+    final productId     = ModalRoute.of(context).settings.arguments as String;
+    final loadedProduct = Provider.of<Products>(context, listen: false).findById(productId);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true, 
         title: Text(
-          'title', 
+          loadedProduct.title, 
           style: Theme.of(context).textTheme.title,
           textAlign: TextAlign.center,
         ),
         backgroundColor: Colors.transparent,
         elevation: 0.0,
-        iconTheme: IconThemeData(color: Colors.black),
+        iconTheme: IconThemeData(
+          color: Colors.black
+        ),
+      leading: new IconButton(
+          icon: Icon(
+            Icons.chevron_left,
+            color: Colors.black,
+            size: 35,
+          ),
+          onPressed: () { Navigator.of(context).pop(); },
+        ),
       ),
     );
   }
