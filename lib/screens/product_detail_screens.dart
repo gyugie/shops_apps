a3ee4ce76be027578e9974f2eb15443a1214de4b
Mugi <mugypleci@gmail.com>
2019-10-20 19:44:54 +0700

    adding hero transition

--- lib/screens/product_detail_screens.dart
+++ lib/screens/product_detail_screens.dart
@@ -37,10 +37,13 @@ class ProductDetailScreen extends StatelessWidget {
             Container(
               height: 300,
               width: double.infinity,
-              child: Image.network(
+              child: Hero(
+                tag: loadedProduct.id,
+                child: Image.network(
                   loadedProduct.imageUrl,
                   fit: BoxFit.cover,
                 ),
+              ),
             ),
             SizedBox(height: 10),
             Container(
